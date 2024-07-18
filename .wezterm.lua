d119fb4a1e101257e595e17abec43021cf47b009
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-07-18 20:58:57 +0800

    Add workspace switcher

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,16 +14,19 @@ config.default_prog = { "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.e
 
 -- Keybindings
 
--- Pane navigation
 config.keys = {
+	-- Pane navigation
 	{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
 	{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
 	{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
 	{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+	-- Adjust pane size
 	{ key = "h", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Left", 5 }) },
 	{ key = "j", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Down", 5 }) },
 	{ key = "k", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Up", 5 }) },
 	{ key = "l", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Right", 5 }) },
+	-- Show the launcher in fuzzy selection mode to list and switch workspaces
+	{ key = "9", mods = "ALT", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
 }
 
 -- and finally, return the configuration to wezterm
